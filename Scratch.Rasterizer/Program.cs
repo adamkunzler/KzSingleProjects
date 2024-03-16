977427564bba055dbec0ae50f0ac16fa22abe8bb
Adam Kunzler <adamkunzler@gmail.com>
2024-03-15 20:42:47 -0500

    add scratch rasterizer project and get lines working

---
+++ Scratch.Rasterizer/Program.cs
@@ -0,0 +1,89 @@
+﻿// https://www.raylib.com/
+// https://github.com/ChrisDill/Raylib-cs
+// dotnet add package Raylib-cs
+
+using Raylib_cs;
+using Scratch.Rasterizer;
+using Color = Raylib_cs.Color;
+
+internal class Program
+{
+    public static void Main()
+    {
+        var screenWidth = 1024;
+        var screenHeight = 1024;
+        var scale = 4;
+
+        //
+        // Initialization
+        //
+        Raylib.InitWindow(screenWidth, screenHeight, ".: Computer Graphics from Scratch - RayTracer :.");
+        Raylib.SetTargetFPS(60);
+
+        //var scene = TestScene();
+        var rasterizer = new Rasterizer(screenWidth, screenHeight, scale);
+
+        //
+        // MAIN RENDER LOOP
+        //
+        while (!Raylib.WindowShouldClose())    // Detect window close button or ESC key
+        {
+            //
+            // PROCESS INPUTS
+            //
+            //TODO
+
+            //
+            // UPDATE STUFF
+            //
+            rasterizer.Update();
+
+            //
+            // RENDER STUFF
+            //
+            rasterizer.Render();
+
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.Black);
+            RenderTextureToWindow(rasterizer.CanvasTexture, screenWidth, screenHeight);
+            Raylib.DrawFPS(10, 10);
+            Raylib.EndDrawing();
+        }
+
+        //raytracer.CleanUp();
+        Raylib.CloseWindow();
+    }
+
+    private static void RenderTextureToWindow(RenderTexture2D target, int windowWidth, int windowHeight)
+    {
+        var src = new Raylib_cs.Rectangle(0, 0, target.Texture.Width, -target.Texture.Height);
+        var dest = new Raylib_cs.Rectangle(0, 0, windowWidth, windowHeight);
+        Raylib.DrawTexturePro(
+            target.Texture,
+            src,
+            dest,
+            new System.Numerics.Vector2(0.0f, 0.0f),
+            0,
+            Color.White);
+    }
+
+    //private static Scene TestScene()
+    //{
+    //    var scene = new Scene();
+
+    //    // lights
+    //    scene.Lights.Add(new Scratch.RayTracer.Lights.AmbientLight(0.2f));
+    //    scene.Lights.Add(new Scratch.RayTracer.Lights.PointLight(0.6f, new Vector3f(2.0f, 1.0f, 0.0f)));
+    //    scene.Lights.Add(new Scratch.RayTracer.Lights.DirectionalLight(0.2f, new Vector3f(1.0f, 4.0f, 4.0f)));
+
+    //    // "sun"
+    //    scene.Spheres.Add(new Sphere(new Vector3f(0, -5001.0f, 0.0f), 5000, new RGBColor(255, 255, 0), 1000.0f, 0.8f));
+
+    //    // objects
+    //    scene.Spheres.Add(new Sphere(new Vector3f(0, -1, 5), 1, new RGBColor(255, 0, 0), 500.0f, 0.2f));
+    //    scene.Spheres.Add(new Sphere(new Vector3f(2, 0, 6), 1, new RGBColor(0, 0, 255), 500.0f, 0.3f));
+    //    scene.Spheres.Add(new Sphere(new Vector3f(-2, 0, 6), 1, new RGBColor(0, 255, 0), 10.0f, 0.4f));
+
+    //    return scene;
+    //}
+}
---
+++ Scratch.Rasterizer/Rasterizer.cs
@@ -0,0 +1,115 @@
+﻿using Kz.Engine.DataStructures;
+using Raylib_cs;
+using System.Windows.Markup;
+
+namespace Scratch.Rasterizer
+{
+    public class Rasterizer
+    {
+        public static readonly RGBColor BACKGROUND_COLOR = new RGBColor(0, 0, 0);
+
+        private Canvas _canvas;
+        private ViewPort _viewport;
+        private Vector3f _camera;
+
+        public RenderTexture2D CanvasTexture => _canvas.Target;
+
+        public Rasterizer(int screenWidth, int screenHeight, int scale)
+        {
+            _canvas = new Canvas(screenWidth / scale, screenHeight / scale);
+            _camera = new Vector3f(0, 0, 0);
+            _viewport = new ViewPort();
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Render()
+        {
+            _canvas.BeginRendering();
+
+            DrawLine(new Vector2f(0, 0), new Vector2f(50, 100), new RGBColor(0, 255, 0));
+            DrawLine(new Vector2f(0, 0), new Vector2f(100, 50), new RGBColor(0, 0, 255));
+
+            _canvas.EndRendering();
+        }
+
+        public void CleanUp()
+        {
+            _canvas.CleanUp();
+        }
+
+        /// <summary>
+        /// Convert canvas coordinates to viewport coordinates
+        /// </summary>
+        public Vector3f CanvasToViewPort(int canvasX, int canvasY)
+        {
+            var vx = canvasX * (_viewport.Width / _canvas.Width);
+            var vy = canvasY * (_viewport.Height / _canvas.Height);
+            var vz = _viewport.DistanceToCamera;
+
+            return new Vector3f(vx, vy, vz);
+        }
+
+        #region Drawing Things
+        
+        private int[] Interpolate(float i0, float d0, float i1, float d1)
+        {
+            if(i0 == i1) 
+            {
+                return [(int)d0];
+            }
+
+            var size = (int)(i1 - i0) + 1;
+            var values = new int[size];
+            var a = (d1 - d0) / (i1 - i0);
+            var d = d0;
+
+            var index = 0;
+            for (var i = i0; i <= i1; i++)
+            {
+                values[index++] = (int)d;
+                d += a;
+            }
+
+            return values;
+        }
+
+        private void DrawLine(Vector2f p0, Vector2f p1, RGBColor color)
+        {
+            if(MathF.Abs(p1.X - p0.X) > MathF.Abs(p1.Y - p0.Y))
+            {
+                // line is horizontal-ish
+                if(p0.X > p1.X)
+                {
+                    (p0, p1) = (p1, p0);
+                }
+
+                var ys = Interpolate(p0.X, p0.Y, p1.X, p1.Y);
+                for(var x = p0.X; x <= p1.X; x++)
+                {
+                    var index = (int)(x - p0.X);
+                    _canvas.PutPixel((int)x, ys[index], color);
+                }
+            }
+            else
+            {
+                // line is vertical-ish
+                if (p0.Y > p1.Y)
+                {
+                    (p0, p1) = (p1, p0);
+                }
+
+                var xs = Interpolate(p0.Y, p0.X, p1.Y, p1.X);
+                for (var y = p0.Y; y <= p1.Y; y++)
+                {
+                    var index = (int)(y - p0.Y);
+                    _canvas.PutPixel(xs[index], (int)y, color);
+                }
+            }
+        }
+
+        #endregion Drawing Things
+    }
+}
